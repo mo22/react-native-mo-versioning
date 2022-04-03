c1dd271803ce26d98a1fc2ee945c63d56924ad75
Moritz Möller <mm@mxs.de>
2022-04-03 18:02:25 +0200

    ..

--- ios/ReactNativeMoVersioning/ReactNativeMoVersioning.m
+++ ios/ReactNativeMoVersioning/ReactNativeMoVersioning.m
@@ -1,4 +1,8 @@
+#if __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
 #import <React/RCTBridgeModule.h>
+#endif
 
 @interface ReactNativeMoVersioning : NSObject <RCTBridgeModule>
 @end
@@ -7,18 +11,19 @@
 
 RCT_EXPORT_MODULE()
 
-- (dispatch_queue_t)methodQueue {
-    return dispatch_get_main_queue();
-}
-
 + (BOOL)requiresMainQueueSetup {
     return YES;
 }
 
-- (NSDictionary *)constantsToExport {
+- (dispatch_queue_t)methodQueue {
+    return dispatch_get_main_queue();
+}
+
+- (NSDictionary*)constantsToExport {
     return @{
-        @"infoDictionary": [[NSBundle mainBundle] infoDictionary]
+        @"infoDictionary": [[NSBundle mainBundle] infoDictionary],
     };
 }
 
 @end
+
