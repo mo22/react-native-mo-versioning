104de5a0f98b4436965fccd0cbf02816d63407ac
Moritz Möller <mm@mxs.de>
2022-04-03 18:44:23 +0200

    ..

--- ios-set-version.sh
+++ ios-set-version.sh
@@ -7,10 +7,13 @@ set -e
 # this will check the parent folders for the installed version...
 node -e 'require("react-native-mo-versioning/lib/ios-set-version.js")'
 
+# jq is not always installed.
 VERSION="$( cat ../package.json | jq -r .version )"
 BUILD="$( echo $VERSION | cut -d . -f 3 )"
 GITREV="$( git rev-parse --short head )$( git diff --quiet || echo '-dirty' )"
 
 /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString '$VERSION'" "$SRCROOT/$INFOPLIST_FILE"
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $BUILD" "$SRCROOT/$INFOPLIST_FILE"
-/usr/libexec/PlistBuddy -c "Set :GitRev $GITREV" "$SRCROOT/$INFOPLIST_FILE"
+
+/usr/libexec/PlistBuddy -c "Set :GitRev $GITREV" "$SRCROOT/$INFOPLIST_FILE" || \
+  /usr/libexec/PlistBuddy -c "Add :GitRev string $GITREV" "$SRCROOT/$INFOPLIST_FILE"
