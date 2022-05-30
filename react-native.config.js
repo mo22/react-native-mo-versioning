b536aa04e3d2807be9969249241ef37ab5944a9c
Moritz Möller <mm@mxs.de>
2022-05-30 13:32:15 +0200

    ..

--- react-native.config.js
+++ react-native.config.js
@@ -1,14 +1,14 @@
+// this is called via main podfile
+// via node ./node_modules/.bin/react-native config
+// requires an xcodeproj in this project as well as a podfile
+
+/** @type {import('@react-native-community/cli-types').Config} */
 module.exports = {
   dependency: {
     platforms: {
       // https://github.com/react-native-community/cli/blob/master/docs/dependencies.md
       ios: {
         scriptPhases: [
-          // {
-          //   name: '[react-native-mo-versioning] Set Version',
-          //   path: './ios-set-version.sh',
-          //   execution_position: 'before_compile',
-          // },
           {
             name: '[react-native-mo-versioning] Set Version',
             path: './versioning-ios.sh',
