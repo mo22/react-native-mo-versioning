93623c0ded547140b28b160e43d355c4d1b94131
Moritz Moeller <mm@mxs.de>
2025-05-07 13:57:36 +0200

    0.1.19

--- versioning-ios.sh
+++ versioning-ios.sh
@@ -1,2 +1,3 @@
-#!/usr/bin/env bash
-node ../node_modules/react-native-mo-versioning/lib/versioning-ios.js
+. "$REACT_NATIVE_PATH/scripts/xcode/with-environment.sh"
+$NODE_BINARY ../node_modules/react-native-mo-versioning/lib/versioning-ios.js
+
